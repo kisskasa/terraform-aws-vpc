3f5986a6c267c68faba450ea5efc786f6e8f0965
kisskasa <chandrasekharkasa@gmail.com>
2025-01-01 19:19:11 +0530

    sg

--- peering.tf
+++ peering.tf
@@ -1,13 +1,13 @@
 resource "aws_vpc_peering_connection" "peering" {
   count = var.is_peering_required ? 1 : 0
   vpc_id        = aws_vpc.main.id # requestor VPC
-  peer_vpc_id = var.acceptor_vpc_id == "" ? data.aws_vpc.default.id : var.acceptor_vpc_id
+  peer_vpc_id   = var.acceptor_vpc_id == "" ? data.aws_vpc.default.id : var.acceptor_vpc_id
   auto_accept = var.acceptor_vpc_id == "" ? true : false
   tags = merge(
     var.common_tags,
     var.vpc_peering_tags,
     {
-        Name = "${local.resource_name}" # expense-dev
+        Name = "${local.resource_name}" #expense-dev
     }
   )
 }
@@ -36,8 +36,7 @@ resource "aws_route" "database_peering" {
 
 resource "aws_route" "default_peering" {
   count = var.is_peering_required && var.acceptor_vpc_id == "" ? 1 : 0
-  route_table_id            = data.aws_route_table.name.id # default vpc route table
+  route_table_id            = data.aws_route_table.main.id # default vpc route table
   destination_cidr_block    = var.vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
 }
-
